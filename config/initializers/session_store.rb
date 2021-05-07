90499716fc3d62ad0e5fd6db729df53a4027b37e
Kizashi Nagata <kizashi1122@gmail.com>
2021-05-08 00:19:51 +0900

    docker redis を参照するように

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,7 +4,7 @@ case Rails.env
 when 'development'
   # 開発環境では localhost の redis にキャッシュする
   Rails.application.config.session_store :redis_store, servers: {
-    host:            'localhost',
+    host:            ENV['REDIS_HOST'] || 'localhost',
     port:            6379,
     connect_timeout: 30, # Defaults to 20 seconds
     read_timeout:    0.2, # Defaults to 1 second
