15d2b71d6fc75617635be60400f6044b9ac119c6
ishiyu <distilledfields@gmail.com>
2021-05-13 18:37:32 +0900

    feat(sessions/show.html.erb) ログイン画面のデザインを追加

--- frontend/entries/login.js
+++ frontend/entries/login.js
@@ -1,1 +1,12 @@
+import "../styles/login.scss"
+
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+// loads the Icon plugin
+UIkit.use(Icons);
+
+// components can be called from the imported UIkit reference
+UIkit.notification('Hello world.');
+
 console.log('login');
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,15 +3,23 @@ const path = require("path")
 
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
+const MODE = 'development';
+
+// ソースマップの利用有無(productionのときはソースマップを利用しない)
+const enabledSourceMap = MODE === 'development';
+
 let entries = {}
-glob.sync("./entries/*.js").map(file => {
-  let name = file.split("/")[2].split(".")[0]
+glob.sync('./entries/*.js').map(file => {
+  let name = file.split('/')[2].split('.')[0]
   entries[name] = file
 })
 
 module.exports = {
-  mode: "development",
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: MODE,
   entry: entries,
+
   plugins: [
     new WebpackManifestPlugin({
       fileName: 'manifest.json',
@@ -23,6 +31,40 @@ module.exports = {
     filename: "javascripts/[name]-[hash].js",
     path: path.resolve(__dirname, "../public/assets"),
   },
+  module: {
+    rules: [{
+      // CSS, Sassファイルの読み込みとコンパイル
+      test: /\.(c|sc)ss$/,
+      use: [{
+        loader: "style-loader", // linkタグに出力する機能
+      }, {
+        loader: "css-loader", // CSSをバンドルするための機能
+        options: {
+          // オプションでCSS内のurl()メソッドを取り込む
+          url: true,
+          // ソースマップの利用有無
+          sourceMap: enabledSourceMap,
+
+          // 0 => no loaders (default);
+          // 1 => postcss-loader;
+          // 2 => postcss-loader, sass-loader
+          importLoaders: 2,
+        },
+      }, {
+        loader: "sass-loader", // Sassをバンドルするための機能
+        options: {
+          // ソースマップの利用有無
+          sourceMap: enabledSourceMap,
+        },
+      }],
+    }, {
+      // 対象となるファイルの拡張子
+      test: /\.(gif|png|jpg|eot|wof|woff|ttf|svg)$/,
+      // 画像をBase64として取り込む
+      type: "asset/inline",
+    }],
+  },
+
 
   devServer: {
     host: '0.0.0.0',
