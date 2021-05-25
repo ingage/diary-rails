d08296377a6cfa1896bc6643a4b3901cc6252906
ishiyu <distilledfields@gmail.com>
2021-05-25 20:49:59 +0900

    refactor(vuetify.ts): vue の設定を plugin フォルダに移動

--- frontend/entries/dashboard.ts
+++ frontend/entries/dashboard.ts
@@ -1,23 +1,11 @@
-import '@mdi/font/css/materialdesignicons.css';
-
 import Vue from 'vue';
-import VueCompositionApi from '@vue/composition-api';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
+import '@p/compositionApi';
+import vuetify from '@p/vuetify';
 
 import Dashboard from '@c/Dashboard.vue';
-
-Vue.use(VueCompositionApi);
-Vue.use(Vuetify);
-
-const v = new Vuetify({
-  icons: {
-    iconfont: 'mdi', // default - only for display purposes
-  },
-});
 
 new Vue({
   el: '#app',
   render: h => h(Dashboard),
-  vuetify: v,
+  vuetify: vuetify,
 });
