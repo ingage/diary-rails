b5857509febde739135082ff1973dcbd9937e3c9
ishiyu <distilledfields@gmail.com>
2021-05-27 23:20:51 +0900

    feat(dashboard.ts): pinia を導入

--- frontend/entries/dashboard.ts
+++ frontend/entries/dashboard.ts
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import '@p/compositionApi';
 import vuetify from '@p/vuetify';
+import pinia from '@p/pinia';
 
-import Dashboard from '@c/Dashboard.vue';
+import Dashboard from '@c/pages/Dashboard.vue';
 
 new Vue({
   el: '#app',
   render: h => h(Dashboard),
+  pinia: pinia,
   vuetify: vuetify,
 });
---
+++ frontend/plugins/pinia.ts
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import { PiniaPlugin, createPinia } from 'pinia';
+
+Vue.use(PiniaPlugin);
+export default createPinia();
